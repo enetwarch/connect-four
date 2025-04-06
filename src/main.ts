17198ec32f3dcb46378903f28337172437b1553f
Enetwarch <enetwarch@protonmail.com>
2025-04-07 07:03:32 +0800

    added local storage saving for player names and colors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,51 @@
 import "./styles/style.css";
 import Player from "./modules/player.ts";
-import Players from "./controllers/observable.ts";
+import Observable from "./controllers/observable.ts";
 import UI from "./controllers/ui.ts";
 import Game from "./controllers/game.ts";
 
-window.addEventListener("load", () => {
-    const players = [
-        new Player("", "red", 1),
-        new Player("", "blue", 2)
-    ];
+type Color = "" | "red" | "blue";
 
-    const observable = new Players(players);
+interface Save {
+    "name": string,
+    "color": Color,
+    "number": number
+};
+
+window.addEventListener("load", () => {
+    const players: Player[] = [];
+
+    const storedPlayers = localStorage.getItem("players");
+    if (!storedPlayers) {
+        players.push(new Player("", "red", 1));
+        players.push(new Player("", "blue", 2));
+    } else {
+        const parsedPlayers: Save[] = JSON.parse(storedPlayers);
+        
+        parsedPlayers.forEach(player => {
+            players.push(new Player(player.name, player.color, player.number));
+        });
+    }
+
+    const observable = new Observable(players);
     
     new UI(observable);
     new Game(observable);
+
+    window.addEventListener("beforeunload", () => {
+        const players: Save[] = [];
+        const sessionPlayers: Player[] = observable.players;
+
+        sessionPlayers.forEach(player => {
+            const save: Save = {
+                "name": player.name,
+                "color": player.color,
+                "number": player.number
+            };
+
+            players.push(save);
+        });
+
+        localStorage.setItem("players", JSON.stringify(players));
+    });
 });
